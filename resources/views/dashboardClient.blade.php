b49f3d72dae15b1c13dc75ca3b969363ce8ebb20
lucasdeaguiarpereira <de.lucas73@gmail.com>
2021-11-05 07:11:13 -0300

    Barra lateral pronta

--- resources/views/dashboardClient.blade.php
+++ resources/views/dashboardClient.blade.php
@@ -13,9 +13,10 @@
     <style type="text/css">
         body, html{height: 100%;}
         .principal{height: 100%;}
-        .barra{width:100%;}
+        .menuSuperior{width:100%;height:70%;background-image: linear-gradient(to right, #35A0A3, #A7CF8D);}
+        .menuLateralReduzido{background-color:#F0EAD2;float:left;width:5% ;height: 100%; min-height: 100%}
         .menuLateral{background-color:#F0EAD2;float:left;width:20% ;height: 100%; min-height: 100%}
-        .bloco2{width:80%;float:left;}
+        .conteudo{width:80%;float:left;background-color:#F2F2F2;height: 100%; min-height: 100%}
         .colorTextLateral{color: #746154;font-weight:400;}
         .nav-item:hover{background-color:#D6CDB7;}
         .button-menu{background-color:#F0EAD2;border:0;}
@@ -24,7 +25,36 @@
 
 <!--begin::Body-->
 <body>
-<!--   
+    
+    <div class="menuLateralReduzido" style="display:none;">
+        <div class="d-flex flex-column flex-shrink-0" style="height:100%;">
+            <button onclick="splitMenu()" class="button-menu p-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </button>
+            
+          
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="nav-item text-center">
+                    <a href="#" class="colorTextLateral nav-link link-dark fs-5" aria-current="page">                            
+                        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-calendar-date-fill" viewBox="0 0 16 16">
+                            <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zm5.402 9.746c.625 0 1.184-.484 1.184-1.18 0-.832-.527-1.23-1.16-1.23-.586 0-1.168.387-1.168 1.21 0 .817.543 1.2 1.144 1.2z"/>
+                            <path d="M16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-6.664-1.21c-1.11 0-1.656-.767-1.703-1.407h.683c.043.37.387.82 1.051.82.844 0 1.301-.848 1.305-2.164h-.027c-.153.414-.637.79-1.383.79-.852 0-1.676-.61-1.676-1.77 0-1.137.871-1.809 1.797-1.809 1.172 0 1.953.734 1.953 2.668 0 1.805-.742 2.871-2 2.871zm-2.89-5.435v5.332H5.77V8.079h-.012c-.29.156-.883.52-1.258.777V8.16a12.6 12.6 0 0 1 1.313-.805h.632z"/>
+                        </svg>
+                    </a>
+                </li>
+                <li class="nav-item mt-3 text-center">
+                    <a href="#" class="colorTextLateral nav-link link-dark fs-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+                            <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+                        </svg>
+                    </a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
     <div class="menuLateral">
         <div class="d-flex flex-column flex-shrink-0" style="height:100%;">
             <button style="align-self: end;" onclick="splitMenu()" class="button-menu p-3">
@@ -61,53 +91,19 @@
             </ul>
         </div>
     </div>
-    <div class="bloco2">
-        <div class="barra">
-            Barra de menu
+    <div class="conteudo">
+        <div class="menuSuperior">
+            <div style="color:white;" class="mx-auto">
+                <h3>Calendário</h3>
+                <h5>Veja o calendario completo</h5>
+            </div>
         </div>
         Area de conteudo:
     </div>
-
-	 -->
-
-    <div class="container-fluid">
-        <div class="row flex-nowrap">
-            <div class="col-auto px-0">
-                <div id="sidebar" class="collapse collapse-horizontal show border-end">
-                    <div id="sidebar-nav" class="list-group border-0 rounded-0 text-sm-start min-vh-100">
-                        <a href="#" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i class="bi bi-bootstrap"></i> <span>Item</span> </a>
-                        <a href="#" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i class="bi bi-film"></i> <span>Item</span></a>
-                        <a href="#" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i class="bi bi-heart"></i> <span>Item</span></a>
-                        <a href="#" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i class="bi bi-bricks"></i> <span>Item</span></a>
-                        <a href="#" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i class="bi bi-clock"></i> <span>Item</span></a>
-                        <a href="#" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i class="bi bi-archive"></i> <span>Item</span></a>
-                        <a href="#" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i class="bi bi-gear"></i> <span>Item</span></a>
-                        <a href="#" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i class="bi bi-calendar"></i> <span>Item</span></a>
-                        <a href="#" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i class="bi bi-envelope"></i> <span>Item</span></a>
-                    </div>
-                </div>
-            </div>
-            <main class="col ps-md-2 pt-2">
-                <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" class="border rounded-3 p-1 text-decoration-none"><i class="bi bi-list bi-lg py-2 p-1"></i> Menu</a>
-                <div class="page-header pt-3">
-                    <h2>Bootstrap 5 Sidebar Menu - Simple</h2>
-                </div>
-                <p class="lead">A offcanvas "push" vertical nav menu example.</p>
-                <hr>
-                <div class="row">
-                    <div class="col-12">
-                        <p>This is a simple collapsing sidebar menu for Bootstrap 5. Unlike the Offcanvas component that overlays the content, this sidebar will "push" the content. Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
-                        <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
-                    </div>
-                </div>
-            </main>
-        </div>
-    </div>
    
-    
-
     {{-- Scripts --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
@@ -118,9 +114,13 @@
             
             console.log($(".menuLateral").css('display'));
             if($(".menuLateral").css('display') == 'block'){
-                $(".menuLateral").fadeOut();
+                $(".menuLateral").hide("slide", {direction: "left" }, 300);
+                $(".menuLateralReduzido").css("display", "block");
+
+                
             }else if($(".menuLateral").css('display') == 'none'){
-                $(".menuLateral").fadeIn();
+                $(".menuLateralReduzido").css("display", "none");
+                $(".menuLateral").show("slide", {direction: "left" }, 300);
             }
         }
     </script>
